bcc8bbf6c8c108a5d77490ed58870f7941ed79bc
Stranger-bot1 <mahatomehulraj27@gmail.com>
2025-07-01 22:41:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 API_ID = 24344133
 API_HASH = "edbe7000baef13fa5a6c45c8edc4be66"
 BOT_TOKEN = "7790100613:AAFrRb6Amtpu5XAos0iuqb85Y05bEXsu_sg"
-ADMIN_CHAT_ID = filesenderandreceiver_bot  # Replace with your Telegram user ID
+ADMIN_CHAT_ID = 7790100613  # Replace with your Telegram user ID
